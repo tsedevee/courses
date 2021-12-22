c6573e0936f13fc4f247f0681fcc65148f160ff5
Mstars_lab1_20 <mstarsacademy@MStarss-iMac-1.local>
2021-12-22 11:44:07 +0800

    Updates

---
+++ precache-manifest.5b84ec9ee0e66fe41d6ddbc70184b9e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ca64b2cecb18ba5a073",
+    "url": "/courses/static/css/main.62953ddb.chunk.css"
+  },
+  {
+    "revision": "1ca64b2cecb18ba5a073",
+    "url": "/courses/static/js/main.1ca64b2c.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/courses/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "b0914f7499e540a17bff",
+    "url": "/courses/static/js/runtime~main.b0914f74.js"
+  },
+  {
+    "revision": "b1fe100c5f813cec21adf3cf2ab45bc7",
+    "url": "/courses/index.html"
+  }
+];
